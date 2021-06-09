c25299c74ffff5d9aab2ee209679718fb6034053
alex <desorbaix@free.fr>
2021-06-09 12:26:28 +0200

    mise en place de binding

--- src/App/App.js
+++ src/App/App.js
@@ -6,23 +6,23 @@ import MemeForm from './components/MemeForm/MemeForm';
  */
 class App extends React.Component {
   //counter=1
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state={counter:0, maChaine:'Hello'};
+    this.state = { current: { titre: '', x: 0, y: 0, text: '', image: 0 } };
   }
-  componentDidUpdate(pprops,pstate){
+  componentDidUpdate(pprops, pstate) {
     console.log(arguments);
     console.log(this.state);
     //Verif et moddif d'etat suite a une modif d'etat (action en chaine) Attention prevenir mles boucles infinies
-    if(this.state.counter===1 && this.state.maChaine!=='new Val'){this.setState({maChaine:'new Val'});}
+    if (this.state.counter === 1 && this.state.maChaine !== 'new Val') { this.setState({ maChaine: 'new Val' }); }
   }
   render() {
     return <div className="App">
       <FlexLayout>
-          <div></div>
-          <MemeForm/>
+        <div></div>
+        <MemeForm onSubmit={formState=>this.setState({current:formState})} />
       </FlexLayout>
-     
+      {JSON.stringify(this.state)}
     </div>;
   }
 }
--- src/App/components/MemeForm/MemeForm.js
+++ src/App/components/MemeForm/MemeForm.js
@@ -1,29 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import styles from './MemeForm.module.css';
 
-const MemeForm = (props) => (
-  <div className={styles.MemeForm} data-testid="MemeForm">
-    <form>
-      <label for="titre">Titre</label><br /><input type="text" id="titre" placeholder="saisissez le titre" />
-      <hr />
-      <label for="image">image</label><br /><select id="image"><option value="1">img.jpg</option></select>
-      <hr />
-      <label for="text">Texte</label><br /><input type="text" id="text" />
-      <div style={{ textAlign: 'center' }}>
-        <div style={{ display: 'inline-block' }}><label for="x">X </label><br /><input type="number" id="x" /></div>
-        <div style={{ display: 'inline-block' }}><label for="y">Y</label><br /><input type="number" id="y" /></div>
-      </div>
-      <hr />
-      <input type="submit">Save</input>
-      <input type="reset">Reset</input>
+const MemeForm = (props) => {
+  const [state, setstate] = useState({ titre: 'bla', x: 10, y: 20, text: 'coucou', image: 1 });
+  return (
+    <div className={styles.MemeForm} data-testid="MemeForm">
+      <form onSubmit={(evt)=>{
+        //annulation du comportement par def. de la soumission d'un formulaire
+        evt.preventDefault();
+        //appele de la func prent envoyé par les props
+        props.onSubmit(state);
+      }}>
+        <label htmlFor="titre">Titre</label><br /><input onChange={evt=>{
+          setstate({...state,titre:evt.target.value});
+        }} value={state.titre} type="text" id="titre" placeholder="saisissez le titre" />
+        <hr />
+        <label htmlFor="image">image</label><br /><select  onChange={evt=>{
+          setstate({...state,image:evt.target.value});
+        }}  value={state.image}  id="image"><option value="1">img.jpg</option></select>
+        <hr />
+        <label htmlFor="text">Texte</label><br /><input type="text" id="text"  onChange={evt=>{
+          setstate({...state,text:evt.target.value});
+        }}   value={state.text}  />
+        <div style={{ textAlign: 'center' }}>
+          <div style={{ display: 'inline-block' }}><label htmlFor="x">X </label><br /><input  onChange={evt=>{
+          setstate({...state,x:evt.target.value});
+        }}   value={state.x} type="number" id="x" /></div>
+          <div style={{ display: 'inline-block' }}><label htmlFor="y">Y</label><br /><input  onChange={evt=>{
+          setstate({...state,y:evt.target.value});
+        }}   value={state.y}  type="number" id="y" /></div>
+        </div>
+        <hr />
+        <input type="submit" value="submit" />
+        <input type="reset" value="reset" />
 
-    </form>
-  </div>
-);
+      </form>
+      {JSON.stringify(state)}
+    </div>
+  );
+}
 
-MemeForm.propTypes = {};
+MemeForm.propTypes = {onSubmit:PropTypes.func.isRequired};
 
-MemeForm.defaultProps = {};
+
 
 export default MemeForm;
