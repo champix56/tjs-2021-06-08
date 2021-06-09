a369f1a06cc71ec329f8e6108c56c54d54e1ff16
alex <desorbaix@free.fr>
2021-06-09 11:20:33 +0200

    ajout d'un disabled par gestion etatique

--- src/App/components/Button/Button.js
+++ src/App/components/Button/Button.js
@@ -1,38 +1,39 @@
-import React,{useState} from 'react';
-import PropTypes  from 'prop-types';
-import  './Button.css';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import './Button.css';
 function Button(props) {
     console.log(props);
-    const [clicked, setclicked] = useState({clicked:false,disabled:false});
-    return <button onClick={evt=>{
-                    //    alert('j\'ai cliqué sur : '+evt.target.innerText);
-                       setclicked({...clicked,clicked:true, disabled:true});
-                       //remise a false de l'etat cliqué du button au bout de 300 ms 
-                       setTimeout(()=>{
-                        setclicked({...clicked,clicked:false});
-                       },300);
-                       setTimeout(()=>{
-                        setclicked({...clicked,disabled:false});
-                       },1000);
-                       console.warn(evt);
-                       props.lorsqueJeClickeraiSurLeButton(evt.target.innerText);
-                    }} 
-                    className={"Button"+(clicked.clicked ? ' click':'')} 
-                    disabled={clicked.disabled?'disabled':''}
-                    style={{
-                        backgroundColor:props.bgcolor,
-                        fontStyle:(props.isItalic?'italic':'normal')
-                    }}
-            >{props.children}</button>;
+    const [clicked, setclicked] = useState({ clicked: false, disabled: false });
+    return <button onClick={evt => {
+        //    alert('j\'ai cliqué sur : '+evt.target.innerText);
+        setclicked({ ...clicked, clicked: true, disabled: true });
+        //remise a false de l'etat cliqué du button au bout de 300 ms 
+        setTimeout(() => {
+            setclicked({ ...clicked, clicked: false });
+            setTimeout(() => {
+                setclicked({ ...clicked, disabled: false });
+            }, 5000);
+        }, 300);
+
+        console.warn(evt);
+        props.lorsqueJeClickeraiSurLeButton(evt.target.innerText);
+    }}
+        className={"Button" + (clicked.disabled ? ' disabled' : '') + (clicked.clicked ? ' click' : '')}
+        disabled={clicked.disabled ? 'disabled' : ''}
+        style={{
+            backgroundColor: props.bgcolor,
+            fontStyle: (props.isItalic ? 'italic' : 'normal')
+        }}
+    >{props.children}</button>;
 }
-Button.propTypes={
-    children:PropTypes.any.isRequired,
-    bgcolor:PropTypes.string.isRequired,
-    lorsqueJeClickeraiSurLeButton:PropTypes.func.isRequired
+Button.propTypes = {
+    children: PropTypes.any.isRequired,
+    bgcolor: PropTypes.string.isRequired,
+    lorsqueJeClickeraiSurLeButton: PropTypes.func.isRequired
 }
-Button.defaultProps={
-    bgcolor:'Aquamarine',
-    isItalic:true,
-    lorsqueJeClickeraiSurLeButton:()=>{}
+Button.defaultProps = {
+    bgcolor: 'Aquamarine',
+    isItalic: true,
+    lorsqueJeClickeraiSurLeButton: () => { }
 }
 export default Button;
