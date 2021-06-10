902f45d8efd925fcec31dab79a38660031580af5
alex <desorbaix@free.fr>
2021-06-10 14:26:59 +0200

    install de bootstrap + react-router-dom+ integration navbar bs3

--- src/App/components/Navbar/Navbar.js
+++ src/App/components/Navbar/Navbar.js
@@ -2,9 +2,27 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './Navbar.module.css';
 
+import  '../../../../node_modules/bootstrap/dist/css/bootstrap.css';
+
+
+
 const Navbar = () => (
-  <div className={styles.Navbar} data-testid="Navbar">
-    Navbar Component
+  <div data-testid="Navbar">
+  
+<nav  className={styles.Navbar+' navbar navbar-inverse'}>
+    <a className="navbar-brand" href="/">Meme.js</a>
+    <ul className="nav navbar-nav">
+        <li className="active">
+            <a href="/thumbnail">Thumbnail</a>
+        </li>
+        <li>
+            <a href="/new">Nouveau meme</a>
+        </li>
+        <li>
+            <a href="/edit/1">Edition meme id:1</a>
+        </li>
+    </ul>
+</nav>
   </div>
 );
 
